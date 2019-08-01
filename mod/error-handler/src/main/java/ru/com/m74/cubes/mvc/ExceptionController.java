0ece8e7093fb0825396a0d55d7d5fbe8527375b1
Maxim Smirnov <m_smirnov@systemres.ru>
2019-08-01 23:04:46 +0300

    Вынес обработку исключительных ситуаций (Exception) в отдельный модуль

---
+++ mod/error-handler/src/main/java/ru/com/m74/cubes/mvc/ExceptionController.java
@@ -0,0 +1,23 @@
+package ru.com.m74.cubes.mvc;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+/**
+ * Обработчик исключительных ситуаций.
+ * Перехватывает все подряд для отображения на UI
+ *
+ * @author mixam
+ * @since 10.03.16 17:17
+ */
+@ControllerAdvice
+public class ExceptionController extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> exceptionHandler(Exception ex) {
+        return new ResponseEntity<>(new ExceptionDTO(ex), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
---
+++ mod/error-handler/src/main/java/ru/com/m74/cubes/mvc/ExceptionDTO.java
@@ -0,0 +1,61 @@
+package ru.com.m74.cubes.mvc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author mixam
+ * @since 21.06.16 12:26
+ */
+public class ExceptionDTO {
+    private String message;
+    private String type;
+    private Long reference;
+    private List<String> stackTrace = new ArrayList<>();
+    private ExceptionDTO cause = null;
+
+    ExceptionDTO(Throwable ex) {
+        message = ex.getMessage();
+        type = ex.getClass().getName();
+
+        for (StackTraceElement el : ex.getStackTrace()) {
+            stackTrace.add(el.toString());
+        }
+
+        if (ex.getCause() != null) {
+            cause = new ExceptionDTO(ex.getCause());
+            reference = cause.reference;
+        } else {
+            reference = System.currentTimeMillis();
+            Logger logger = LoggerFactory.getLogger(getClass());
+            logger.error(reference + ":", ex);
+        }
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Long getReference() {
+        return reference;
+    }
+
+    public boolean isSuccess() {
+        return false;
+    }
+
+    public List<String> getStackTrace() {
+        return stackTrace;
+    }
+
+    public ExceptionDTO getCause() {
+        return cause;
+    }
+}
