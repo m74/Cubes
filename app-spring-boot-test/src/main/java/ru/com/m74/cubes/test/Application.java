08779b1a8550b001d450f7605af5ac6b3a7bd91e
Maxim Smirnov <m_smirnov@systemres.ru>
2019-08-01 14:04:50 +0300

    Добавил пример приложения без ORM

---
+++ app-spring-boot-test/src/main/java/ru/com/m74/cubes/test/Application.java
@@ -0,0 +1,13 @@
+package ru.com.m74.cubes.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication(scanBasePackages = "ru.com.m74.cubes")
+@EntityScan(basePackages = "ru.com.m74.cubes")
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
