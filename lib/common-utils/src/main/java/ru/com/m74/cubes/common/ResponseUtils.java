c737baab6a4c020e2c905a24e00441f50b5c015b
Maxim Smirnov <m_smirnov@systemres.ru>
2019-08-06 10:25:18 +0300

    Добавил утилиты для записи бинарных данных в response

---
+++ lib/common-utils/src/main/java/ru/com/m74/cubes/common/ResponseUtils.java
@@ -0,0 +1,34 @@
+package ru.com.m74.cubes.common;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+public class ResponseUtils {
+
+    public static void write(HttpServletResponse response, byte[] data) throws IOException {
+        response.setContentLength(data.length);
+        ServletOutputStream out = response.getOutputStream();
+        out.write(data);
+    }
+
+    public static void setResponseFileName(HttpServletResponse response, String fileName, boolean inLine) {
+        try {
+            response.setHeader("Content-Disposition", (inLine ? "inline" : "attachment") + "; filename*=UTF-8''" + URLEncoder.encode(fileName, "UTF-8").replace("+", "%20"));
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+
+        }
+    }
+
+    public static void setPdfHeaders(HttpServletResponse response) {
+        response.setHeader("Content-Type", "application/pdf");
+        response.setHeader("X-Frame-Options", "SAMEORIGIN");
+        response.setHeader("Pragma", "No-cache");
+        response.setHeader("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate");
+        response.setDateHeader("Expires", 0);
+    }
+
+}
