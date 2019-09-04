f90f7e77508206fb304d87d648b17a7ecdb1306c
Maxim Smirnov <m_smirnov@systemres.ru>
2019-09-04 09:35:39 +0300

    fix bug: при инициализации value: new Date() первое событие 'change' не корректное

--- mod/mod-app/src/main/webapp/overrides/form/field/Date.js
+++ mod/mod-app/src/main/webapp/overrides/form/field/Date.js
@@ -2,8 +2,14 @@ Ext.define('overrides.form.field.Date', {
     override: 'Ext.form.field.Date',
 
     dateFormat: 'c',
-    format: 'd.m.Y'
-    // startDay: 1,
+    format: 'd.m.Y',
+    startDay: 1,
+
+    initValue: function (value) {
+        this.callParent(arguments);
+        // fix bag
+        this.lastValue = this.rawValue
+    }
     // maskRe: /[\d\.]/,
 
     // enableKeyEvents: true,
