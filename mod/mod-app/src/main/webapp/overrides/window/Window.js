ae6d7562ef27aff2b23137261247f72fd5362ce5
Maxim Smirnov <m_smirnov@systemres.ru>
2019-09-05 11:38:01 +0300

    предотвращение размера окна превышающего размер экрана

---
+++ mod/mod-app/src/main/webapp/overrides/window/Window.js
@@ -0,0 +1,25 @@
+/**
+ * overrides.window.Window
+ *
+ * @author mixam
+ * @date 04/09/2019
+ */
+Ext.define('overrides.window.Window', {
+    override: 'Ext.window.Window',
+    initComponent: function () {
+        var me = this;
+        this.callParent(arguments);
+
+        if (this.layout.type === 'fit' && this.items.length) {
+            this.items.get(0).setAutoScroll(true);
+        }
+
+        this.on('resize', function (cmp, w, h) {
+            var vh = Ext.getBody().getViewSize().height;
+            if (vh < h) {
+                me.setHeight(vh - 20);
+            }
+            this.center();
+        });
+    }
+});
