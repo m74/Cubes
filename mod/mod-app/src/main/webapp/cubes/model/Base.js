b8703b7e961987560d9b7376b9e5abe0d84959d0
Maxim Smirnov <m_smirnov@systemres.ru>
2019-09-14 21:46:12 +0300

    fix: {rootProperty: "records"} мешает нормальной загрузке деревьев. Не считываются children. куча глюков. Убрал вообще нафиг "schema".. не разобрался пока с ней толком

--- mod/mod-app/src/main/webapp/cubes/model/Base.js
+++ mod/mod-app/src/main/webapp/cubes/model/Base.js
@@ -5,25 +5,25 @@
  * @date 26.09.2018
  */
 Ext.define('Ext.cubes.model.Base', {
-    extend: 'Ext.data.Model',
-    identifier: 'negative',
-    schema: {
-        namespace: 'Ext.cubes.model',
-        proxy: {
-            type: 'rest',
-            url: '{entityName}',
-            reader: {
-                type: 'json',
-                successProperty: 'success',
-                totalProperty: 'totalCount',
-                messageProperty: 'message',
-                rootProperty: 'records'
-            },
-            writer: {
-                type: 'json',
-                writeAllFields: false
-                // root: 'changes'
-            }
-        }
-    }
+    extend: 'Ext.data.Model'
+    // identifier: 'negative',
+    // schema: {
+    //     namespace: 'Ext.cubes.model',
+    //     proxy: {
+    //         type: 'rest',
+    //         url: '{entityName}',
+    //         reader: {
+    //             type: 'json',
+    //             successProperty: 'success',
+    //             totalProperty: 'totalCount',
+    //             messageProperty: 'message'
+    //             // rootProperty: 'records'
+    //         },
+    //         writer: {
+    //             type: 'json',
+    //             writeAllFields: false
+    //             // root: 'changes'
+    //         }
+    //     }
+    // }
 });
