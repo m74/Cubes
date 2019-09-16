8b649e9a3c505c663dadcdc32922efb4e627f0a4
Maxim Smirnov <m_smirnov@systemres.ru>
2019-09-16 11:13:39 +0300

    перенес метадатнные в innerClass

--- lib/extjs-dto/src/main/java/ru/com/m74/extjs/dto/Response.java
+++ lib/extjs-dto/src/main/java/ru/com/m74/extjs/dto/Response.java
@@ -49,4 +49,37 @@ public class Response<T> {
     public MetaData getMetaData() {
         return metaData;
     }
+
+    class MetaData {
+        private final String root = "records";
+        private final String idProperty = "id";
+        private final String id = "id";
+        private final String totalProperty = "totalCount";
+        private final String successProperty = "success";
+        private final String messageProperty = "message";
+
+        public String getRoot() {
+            return root;
+        }
+
+        public String getIdProperty() {
+            return idProperty;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public String getTotalProperty() {
+            return totalProperty;
+        }
+
+        public String getSuccessProperty() {
+            return successProperty;
+        }
+
+        public String getMessageProperty() {
+            return messageProperty;
+        }
+    }
 }
