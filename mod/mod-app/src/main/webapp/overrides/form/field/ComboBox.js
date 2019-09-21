8f14b966a1d823b43aa2cc89cb838e19e905b1fb
Maxim Smirnov <m_smirnov@systemres.ru>
2019-09-21 18:18:07 +0300

    fix autoLoad store bug

--- mod/mod-app/src/main/webapp/overrides/form/field/ComboBox.js
+++ mod/mod-app/src/main/webapp/overrides/form/field/ComboBox.js
@@ -5,7 +5,11 @@ Ext.define('overrides.form.field.ComboBox', {
     override: 'Ext.form.field.ComboBox',
     valueField: 'value',
     setValue: function (v) {
-        if (v && this.store.find(this.valueField, v[this.valueField], 0, false, false, true) === -1) {
+        if (this.autoLoad && !this.store.isLoaded()) {
+            this.store.on('load', function () {
+                this.setValue(v)
+            }, this, {single: true});
+        } else if (v && this.store.find(this.valueField, v[this.valueField], 0, false, false, true) === -1) {
             this.setSelection(this.store.add(v));
         }
         this.callParent([v]);
