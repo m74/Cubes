2fd0e66c37680f6d5c8d7302703c2172301d4175
Maxim Smirnov <m_smirnov@systemres.ru>
2019-10-29 14:15:37 +0300

    фильтр период

--- lib/jdbc-tools/src/main/java/ru/com/m74/cubes/jdbc/utils/EMUtils.java
+++ lib/jdbc-tools/src/main/java/ru/com/m74/cubes/jdbc/utils/EMUtils.java
@@ -14,12 +14,16 @@ import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import static java.util.Objects.requireNonNull;
 import static ru.com.m74.cubes.common.ObjectUtils.*;
+import static ru.com.m74.cubes.jdbc.utils.DTOUtils.findField;
 import static ru.com.m74.cubes.jdbc.utils.SqlUtils.getColumnNameWithAlias;
 import static ru.com.m74.cubes.jdbc.utils.SqlUtils.getResultSetFieldName;
+import static ru.com.m74.extjs.dto.Filter.Operator.between;
 
 public class EMUtils {
     private static final String TIMESTAMP_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
@@ -38,9 +42,15 @@ public class EMUtils {
             String fname = filter.getProperty();
             String pname = fname + i.getAndIncrement();
 
-            Field field = DTOUtils.findField(q.getType(), fname);
+            Field field = findField(q.getType(), fname);
             if (field != null) {
-                params.put(pname, cast(filter.getValue(), field.getType()));
+                if (filter.getOperator() == between) {
+                    Date[] period = requireNonNull(cast(filter.getValue(), Date[].class), "Не указан период");
+                    params.put(pname + "From", period[0]);
+                    params.put(pname + "Till", period[1]);
+                } else {
+                    params.put(pname, cast(filter.getValue(), field.getType()));
+                }
 
                 String cname = getColumnNameWithAlias(q.getType(), field);
 
@@ -75,7 +85,13 @@ public class EMUtils {
                             break;
                     }
                 } else if (Date.class.isAssignableFrom(field.getType()) || Number.class.isAssignableFrom(field.getType())) {
-                    q.and(cname + " " + filter.getOperator().sql() + " :" + pname);
+                    switch (filter.getOperator()) {
+                        case between:
+                            q.and(cname + " between :" + pname + "From and :" + pname + "Till");
+                            break;
+                        default:
+                            q.and(cname + " " + filter.getOperator().sql() + " :" + pname);
+                    }
                 } else {
                     throw new RuntimeException(field.getName());
                 }
@@ -158,8 +174,17 @@ public class EMUtils {
             return null;
         }
 
-        if (!(value instanceof String || value instanceof Number || value instanceof Boolean)) {
-            throw new RuntimeException("Not a string, number or boolean value: " + value);
+//        if (!(value instanceof String || value instanceof Number || value instanceof Boolean)) {
+//            throw new RuntimeException("Not a string, number or boolean value: " + value);
+//        }
+
+        if (cls.equals(Date[].class)) {
+            List list = (List) value;
+            Date[] arr = new Date[list.size()];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = cast(list.get(i), Date.class);
+            }
+            return (T) arr;
         }
 
         String strVal = value.toString();
@@ -172,6 +197,7 @@ public class EMUtils {
                 throw new RuntimeException("Ошибка при конвертации строки в дату", e);
             }
         }
+
         if (cls.equals(Long.class) || cls.equals(long.class)) {
             try {
                 if (value instanceof Double) {
