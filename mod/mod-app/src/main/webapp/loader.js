45786550ae370355b9f2392ac342b339843bb4f3
Maxim Smirnov <m_smirnov@systemres.ru>
2019-11-28 23:19:16 +0300

    + сделал чтобы установленная пользователем тема сохранялась, а то при открытии программы её приходится заново выбирать

--- mod/mod-app/src/main/webapp/loader.js
+++ mod/mod-app/src/main/webapp/loader.js
@@ -1,13 +1,13 @@
 String.prototype.apply = function (values) {
     return this.replace(/{[_a-zA-Z][_a-zA-Z0-9.:*(" )]*}/g, function (key) {
         key = key.substring(1, key.length - 1);
-        var arr = key.split(':');
+        const arr = key.split(':');
         key = arr.shift();
-        var fn = arr.join(':');
-        var val = (values !== undefined && values[key] !== undefined) ? values[key] : '';
+        const fn = arr.join(':');
+        let val = (values !== undefined && values[key] !== undefined) ? values[key] : '';
         if (fn) {
-            var fname = fn.replace(/\(.*/, '');
-            var args = (fn === fname) ? [] : eval('[' + fn.replace(/(.*\()|(\).*)/g, '') + ']');
+            const fname = fn.replace(/\(.*/, '');
+            const args = (fn === fname) ? [] : eval('[' + fn.replace(/(.*\()|(\).*)/g, '') + ']');
             args.unshift(val);
             val = Ext.util.Format[fname].apply(Ext.util.Format, args);
         }
@@ -16,43 +16,17 @@ String.prototype.apply = function (values) {
 };
 
 
-var Loader = new function () {
-    function getQ() {
-        var script = document.getElementsByTagName("script");
-        for (var i = 0; i < script.length; i++) {
-            var src = script[i].src;
-            if (src && src.match(/^.*loader.js.*$/))
-                return src.split("?")[1];
-        }
-        return null;
-    }
-
-    function parseQ(query) {
-        var params = {};
-        var seg = query.replace(/^\?/, '').split('&');
-        for (var i = 0; i < seg.length; i++) {
-            if (!seg[i])
-                continue;
-            var s = seg[i].split('=');
-            params[s[0]] = s[1] ? s[1] : true;
-        }
-        return params;
-    }
-
-    var params =
-        Object.assign(parseQ(getQ()), parseQ(location.search));
-    if (params.debug) params.suffix = "-debug";
-
+const Loader = new function () {
     function load(c) {
         if (typeof (c) === 'string')
             c = {
                 src: c
             };
-        c.src = c.src.apply(params);
+        // c.src = c.src.apply(params);
 
         if (c.src.match('\.css$')) {
-            document.write('<link rel="stylesheet" type="text/css" href="' + c.src + '" />');
-            // var el = document.createElement("link");
+            document.write(`<link rel="stylesheet" type="text/css" href="${c.src}" />`);
+            // const el = document.createElement("link");
             // el.setAttribute("href", str);
             // el.setAttribute("type", "text/css");
             // el.setAttribute("rel", "stylesheet");
@@ -77,19 +51,52 @@ var Loader = new function () {
     }
 
     this.load = function (arr) {
-        for (var i = 0; i < arr.length; i++) {
+        for (let i = 0; i < arr.length; i++) {
             load(arr[i]);
         }
     };
 };
 
+const params = (function () {
+    function getQ() {
+        const script = document.getElementsByTagName("script");
+        for (let i = 0; i < script.length; i++) {
+            const src = script[i].src;
+            if (src && src.match(/^.*loader.js.*$/))
+                return src.split("?")[1];
+        }
+        return null;
+    }
+
+    function parseQ(query) {
+        const params = {};
+        const seg = query.replace(/^\?/, '').split('&');
+        for (let i = 0; i < seg.length; i++) {
+            if (!seg[i])
+                continue;
+            const s = seg[i].split('=');
+            params[s[0]] = s[1] ? s[1] : true;
+        }
+        return params;
+    }
+
+    return Object.assign(parseQ(getQ()), parseQ(location.search));
+})();
+
+if (!localStorage.theme) localStorage.theme = 'Triton';
+
+const
+    suffix = params.debug ? '-debug' : '',
+    theme = localStorage.theme.toLowerCase(),
+    lang = params || 'ru';
+
 Loader.load([
-    'ext/ext-all{suffix}.js',
-    'ext/packages/ux/classic/ux{suffix}.js',
-    'ext/classic/theme-{theme}/theme-{theme}{suffix}.js',
-    'ext/classic/theme-{theme}/resources/theme-{theme}-all{suffix}.css',
-    'ext/packages/font-awesome/resources/font-awesome-all{suffix}.css',
-    'ext/classic/locale/locale-{lang}{suffix}.js',
-    'app.js'
+    `ext/ext-all${suffix}.js`,
+    `ext/packages/ux/classic/ux${suffix}.js`,
+    `ext/classic/theme-${theme}/theme-${theme}${suffix}.js`,
+    `ext/classic/theme-${theme}/resources/theme-${theme}-all${suffix}.css`,
+    `ext/packages/font-awesome/resources/font-awesome-all${suffix}.css`,
+    `ext/classic/locale/locale-${lang}${suffix}.js`,
+    `app.js`
 ]);
 
