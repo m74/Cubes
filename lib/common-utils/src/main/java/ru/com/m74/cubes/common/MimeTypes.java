d8acc35b7bb102bb4ec849d76b9392f90f366f6f
Maxim Smirnov <mxsmir@gmail.com>
2020-04-16 22:02:48 +0300

    exclude *.psd resources remove MimeTypes interface

--- lib/common-utils/src/main/java/ru/com/m74/cubes/common/MimeTypes.java
+++
@@ -1,6 +0,0 @@
-package ru.com.m74.cubes.common;
-
-public interface MimeTypes {
-    public static final String XLSX = "application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml";
-    public static final String DOCX = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
-}
