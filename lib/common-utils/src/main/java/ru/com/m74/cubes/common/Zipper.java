4ef0004ef2115b91d0df6bb1b08efeb80e3b9f20
Maxim Smirnov <mxsmir@gmail.com>
2020-04-17 20:20:31 +0300

    Добавлять индекс, при дублировании имени

--- lib/common-utils/src/main/java/ru/com/m74/cubes/common/Zipper.java
+++ lib/common-utils/src/main/java/ru/com/m74/cubes/common/Zipper.java
@@ -1,6 +1,11 @@
 package ru.com.m74.cubes.common;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -8,15 +13,35 @@ import static org.apache.commons.io.IOUtils.copy;
 
 public class Zipper extends ZipOutputStream {
 
+    private static final Logger logger = LoggerFactory.getLogger(Zipper.class);
+
     public Zipper(OutputStream out) {
         super(out);
     }
+
+    private Map<String, Integer> names = new HashMap<>();
 
     public void addEntry(String fileName, File file) throws IOException {
         addEntry(fileName, new FileInputStream(file));
     }
 
     public void addEntry(String fileName, InputStream in) throws IOException {
+        Integer count = names.get(fileName);
+        if (count == null) {
+            names.put(fileName, 1);
+        } else {
+            names.put(fileName, count + 1);
+
+            int i = fileName.lastIndexOf('.');
+            if (i != -1) {
+                fileName = fileName.substring(0, i) + " (" + count + ")" + fileName.substring(i);
+            } else {
+                fileName = fileName + " (" + count + ")";
+            }
+        }
+
+        logger.debug("add entity: {}", fileName);
+
         ZipEntry zipEntry = new ZipEntry(fileName);
         putNextEntry(zipEntry);
         copy(in, this);
