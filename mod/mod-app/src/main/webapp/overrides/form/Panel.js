477cf90076af59462703dceaf30e4fd7dddea458
Maxim Smirnov <mxsmir@gmail.com>
2020-10-08 13:15:41 +0300

    Теперь можно передавать только конфиг вместо экземпляра. {} вместо new Model({})

--- mod/mod-app/src/main/webapp/overrides/form/Panel.js
+++ mod/mod-app/src/main/webapp/overrides/form/Panel.js
@@ -2,7 +2,12 @@ Ext.define('overrides.form.Panel', {
     override: 'Ext.form.Panel',
     initComponent: function () {
         this.callParent(arguments);
-        if (this.record) this.loadRecord(this.record);
+        if (this.record) {
+            if (!this.record.isModel && this.model) {
+                this.record = this.model.create(this.record);
+            }
+            this.loadRecord(this.record);
+        }
         if (this.values) this.form.setValues(this.values);
     }
 });
