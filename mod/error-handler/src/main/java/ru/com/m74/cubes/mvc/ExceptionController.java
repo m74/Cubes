308fd1f21c11e8284b193081925489ce729df894
Smirnov Maxim <mxsmir@gmail.com>
2023-02-22 11:33:52 +0300

    ResponseStatusException

--- mod/error-handler/src/main/java/ru/com/m74/cubes/mvc/ExceptionController.java
+++ mod/error-handler/src/main/java/ru/com/m74/cubes/mvc/ExceptionController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 /**
@@ -18,6 +19,10 @@ public class ExceptionController extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> exceptionHandler(Exception ex) {
-        return new ResponseEntity<>(new ExceptionDTO(ex), HttpStatus.INTERNAL_SERVER_ERROR);
+        if (ex instanceof ResponseStatusException) {
+            return new ResponseEntity<>(ex, ((ResponseStatusException) ex).getStatus());
+        } else {
+            return new ResponseEntity<>(new ExceptionDTO(ex), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
